414d898962e7b2b18ba23c0ed08bbc0f1bbab3df
Piotr Karpala <piotrkarpala@microsoft.com>
2024-08-01 00:30:41 -0400

    init commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const { RepositoryManager } = require('./repositoryManager')
+const { toCSV } = require('./csvHelper')
 
 /**
  * The main function for the action.
@@ -7,18 +8,16 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const org = core.getInput('github-org', { required: true })
+    const token = core.getInput('github-token', { required: true })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repoManager = new RepositoryManager(token)
+    const repositories = await repoManager.getRepositories(org, token)
+    const results = await repoManager.processRepositories(repositories)
+    const fileName = toCSV(results, org)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('file', fileName)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
